248a44f3c4aa484cad19dc9f15acd38fa1f5a22b
Alex Mancheno <alex.mancheno@gmail.com>
2016-09-22 13:18:15 -0400

    update

--- 8NumberCross/Hw8.cpp
+++ 8NumberCross/Hw8.cpp
@@ -1,13 +1,18 @@
+//Mancheno, Alex
+//CS 211 - Section 52B
+//Hw # 8
 #include <iostream>
 #include <cmath>
 using namespace std;
 
 bool valid(int q[], int c) {
-    for (int col = 0; col < c; col++) {
-        if (q[col] == q[c]) {
-            return false;
-        }
-    }
+    //this for-loop checks if the number already is on the board:
+    for (int col = 0; col < c; col++) 
+        if (q[col] == q[c]) return false;
+    /*following block of if/elif statements contains the tests for each
+    individual column to check whether the difference between our current column
+    and any adjacent column is less than 2:
+    */
     if (c == 1) {
         if (abs(q[c] - q[0]) < 2) return false;      
     } else if (c == 2) {
@@ -33,11 +38,12 @@ bool valid(int q[], int c) {
         else if (abs(q[c] - q[5]) < 2) return false; 
         else if (abs(q[c] - q[6]) < 2) return false;         
     }
-    return true;
+    return true; //method returns true if none of our tests fail.
 }
 
 void print(int q[], int &s) {
     s++;
+    //to print our board:
     cout << "Solution " << s << ": " << endl; 
     cout << "   " << q[1] << " " << q[6] << endl;
     cout << " " << q[0] << " " << q[2] << " " << q[5] << " " << q[7] << endl;
@@ -45,9 +51,16 @@ void print(int q[], int &s) {
 }
 
 int main() {
-    int q[8] = {100, 100, 100, 100, 100, 100, 100, 100}, c = 0, numberOfSolutions = 0;
+    //we set every column to our board 'q' an arbitrary number that does not
+    //interfere with the tests from our 'valid' method above:
+    int q[8] = {100, 100, 100, 100, 100, 100, 100, 100},
+    c = 0, numberOfSolutions = 0;
+    //we set first column to 0 so the following while loop begins at 1 correctly:
     q[0] = 0;
-
+    /*this while-loop contains a similar algorithm to the 8queen problems; we 
+     increment row if we cannot place the number there, increment column when we 
+     can, backtrack if we cannot increment column, print when we find a solution,
+     and end the program when all solutions are found:*/
     while (true) {
         q[c]++;
         if (c == -1) {
