82cdb58ef8edd76cdeae5e128b1ee30682ba9446
Alex Mancheno <alex.mancheno@gmail.com>
2016-10-22 13:20:57 -0400

    done

--- HowManySteps/Hw17.cpp
+++ HowManySteps/Hw17.cpp
@@ -1,63 +1,72 @@
-#include <cstdlib>
+//Mancheno, Alex
+//CS 211 - Section 52B
+//Hw # 17 - How many steps taken?
+
 #include <fstream>
 #include <iostream>
 #include <vector>
 #include <sstream>
-#include <cstring>
 using namespace std;
-
-//void swap(int q, )
 
 int main() {
 
     ifstream myFile;
     myFile.open("class.txt");
-    int numOfClasses, studentsHeight, numberOfStepsTaken = 0;
+    
+    int numOfClasses, studentsHeight, stepsTaken = 0;
     vector<int> students;
 
     if (myFile != 0) {
         cout << "File opened successfully!" << endl;
-        string line = "";
+        string line;
         getline(myFile, line);
         istringstream iss(line);
         iss >> numOfClasses;
         cout << numOfClasses << endl;
 
         int i, j, k;
+        
+        //to get heights from current class:
         for (i = 0; i < numOfClasses; i++) {
-            getline(myFile, line);
-            
-            for (i = 0; i < line.length(); i++) {
-                
+            getline(myFile, line, ' ');
+            cout << line;
+            for (j = 0; j < 19; j++) {
+                getline(myFile, line, ' ');
+                istringstream iss(line);
+                iss >> studentsHeight;
+                students.push_back(studentsHeight);
             }
             
-            /* for (j = 0; j < ; j++) {
-                getline(myFile, number, ' ');
-                cout << number << " ";
-                //istringstream iss(line);
-                //iss >> studentsHeight;
-                //cout << studentsHeight << " ";
-                //students.push_back(studentsHeight);
-            }*/
-
-            /*for (int j = 0; j < 20; j++)
+            //to get last height number from current class without problems:
+            getline(myFile, line, '\n');
+            istringstream iss(line);
+            iss >> studentsHeight;
+            students.push_back(studentsHeight);
+            
+            cout << " (before sort): ";
+            for (j = 0; j < 20; j++)
                 cout << students[j] << " ";
             cout << endl;
-            for (j = 0; j < 19; j++) {
-                if (students[j] > students[j + 1]) {
-                    for (k = j - 1; k >= 0; k--) {
-                        if (students[j] < students[k]) {
-                            students.insert(students.begin() + k, students[j]);
-                            numberOfStepsTaken += j + 1;
-                        }
-                    }
+
+            //sort:
+            for (j = 0; j < 20; j++) {
+                if (students[j] < students[0]) {
+                    int temp = students[j];
+                    students.erase(students.begin() + j);
+                    students.insert(students.begin(), temp);
+                    stepsTaken += j;
                 }
             }
-             */
-            //cout << numberOfStepsTaken << endl;
+                     
+            cout << i + 1 << " (after sort):  ";
+            for (j = 0; j < 20; j++)
+                cout << students[j] << " ";
             cout << endl;
-            //students.clear();
-            //numberOfStepsTaken = 0;
+
+            cout << "number of steps taken: " << stepsTaken << endl;
+            cout << endl;
+            students.clear();
+            stepsTaken = 0;
         }
 
         myFile.close();
