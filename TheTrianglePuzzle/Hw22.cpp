01bbf9c23b06d49dc3418e2f4aa04a53c50b3f08
Alex Mancheno <alex.mancheno@gmail.com>
2016-12-07 23:56:03 -0500

    done

--- TheTrianglePuzzle/Hw22.cpp
+++ TheTrianglePuzzle/Hw22.cpp
@@ -14,10 +14,10 @@ void readFromFile() {
     file.open("triangle.txt");
     string line;
     int r = 0, c = 0;
-    while (getline(file, line)) {
+    while (getline(file, line)) { //read each line
         istringstream iss(line);
         int n;
-        while (iss >> n) {
+        while (iss >> n) { //read each number from every line
             inputArray[r][c] = n;
             c++;
         }
@@ -32,26 +32,37 @@ int max(int a, int b) {
 }
 
 int calculateCost(int r, int c) {
+    //stop this recursive ladder if we are out of bounds:
     if (r < 0 || c < 0) return 0;
     
+    //stop this recursive ladder if cost has been calculated:
     if (cost[r][c] != 0) return cost[r][c];
     
+    //start calculating cost-matrix starting from the bottom via recursion:
     int rightSide = calculateCost(r - 1, c) + inputArray[r][c];
     int leftSide = calculateCost(r - 1, c - 1) + inputArray[r][c];
     
+    //fill cost-matrix once recursive calculations are finished:
     cost[r][c] = max(rightSide, leftSide);
     return cost[r][c];
 }
 
-void print(int a[][col]) {
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; a[i][j] != -1; j++) {
-            if (a[i][j] < 10) cout << a[i][j] << "  ";
-            else              cout << a[i][j] << " ";
-        }
-        cout << endl;
+void printMaxPath(int r, int c) {
+    //recursive print of "maximum path":
+    if (r == 0) {
+        return; //return if we've run out of rows to print
     }
-    cout << endl;
+    if (cost[r - 1][c] > cost[r - 1][c - 1]) { 
+        //print upper-left side of inputArray if cost to that side is higher:
+        printMaxPath(r - 1, c);
+        if (r % 10 == 0) cout << endl;
+        cout << inputArray[r - 1][c] << " ";
+    } else {
+        //print upper-right side of inputArray if cost to that side is higher:
+        printMaxPath(r - 1, c - 1);
+        if (r % 10 == 0) cout << endl;
+        cout << inputArray[r - 1][c - 1] << " ";
+    }    
 }
 
 int main() {
@@ -62,21 +73,30 @@ int main() {
         for (c = 0; c < col; c++)
             inputArray[r][c] = -1;      
 
-    //read numbers from file into 'input' array:
-    readFromFile();
-    
-    print(inputArray);
+    //read numbers from file and store into 'inputArray':
+    readFromFile();   
     
     int lastRow[col] = {0};
     
-    for (int i = 0; i < col; i++) {
+    //to calculate 'cost' matrix using 'inputArray':
+    for (int i = 0; i < col; i++) 
         lastRow[i] = calculateCost(row - 1, i);
-    }
-    int max = lastRow[0];
+    
+    //to find max cost and keep track of position on last row of cost matrix:
+    int max = lastRow[0], indexOfMax;
     for (int i = 1; i < col; i++) {
-        if (lastRow[i] > max) max = lastRow[i];
+        if (lastRow[i] > max) {
+            max = lastRow[i];
+            indexOfMax = i;
+        }
     }
-    cout << max << endl;
+    
+    //to print sequence:
+    cout << "Sequence of \"maximum path\": " << endl << endl;    
+    printMaxPath(row - 1, indexOfMax);
+    cout << inputArray[row - 1][indexOfMax];
+    
+    cout << endl << endl << "The value of the maximum sum is: " << max << endl;
     return 0;
 }
 
